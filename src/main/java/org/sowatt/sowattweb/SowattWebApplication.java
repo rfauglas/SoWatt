44c4ea106a75c03e89ebb6310c10b80162f591e7
Ronan <rfauglas@listo.pro>
2019-12-11 11:16:53 +0100

    init

---
+++ src/main/java/org/sowatt/sowattweb/SowattWebApplication.java
@@ -0,0 +1,13 @@
+package org.sowatt.sowattweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SowattWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SowattWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sowatt/sowattweb/SowattWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sowatt.sowattweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SowattWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
